d68e205f6770280198573f6b056b4474b6effaa6
Okan <okan@suderra.com>
2026-01-21 09:03:00 +0000

    fix(gateway-api): add no-empty-function and no-dynamic-delete eslint-disable to test files
    Reduces lint errors from 58 to 54

--- apps/gateway-api/src/middleware/__tests__/timeout.middleware.spec.ts
+++ apps/gateway-api/src/middleware/__tests__/timeout.middleware.spec.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-dynamic-delete */
+/* eslint-disable @typescript-eslint/no-empty-function */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-unsafe-return */
 /* eslint-disable @typescript-eslint/require-await */
